2d3ff17d5d0fc27d41fe8b581dccb697010b7d57
Vladislav <vladstar1606@mail.ru>
2024-07-31 22:56:35 +0200

    removed source rect and replaced DrawTextureRec->DrawTexture

--- dino.c
+++ dino.c
@@ -42,7 +42,6 @@ typedef struct {
     Vector2 pos;
     Vector2 vel;
     Texture2D dino_frame_loop[DINO_FRAMES_NUM];
-    Rectangle dino_source_rect;
     Rectangle dino_pos_rect;
 } Dino;
 
@@ -50,7 +49,6 @@ typedef struct {
     Vector2 size;
     Vector2 pos;
     Vector2 vel;
-    Rectangle cactus_source_rect;
     Texture2D asset;
     Rectangle cactus_pos_rect;
 } Cactus;
@@ -59,7 +57,6 @@ typedef struct {
     Vector2 pos;
     Vector2 vel;
     Texture2D ground_frame;
-    Rectangle ground_source_rect;
 } Ground;
 
 void load_textures(const char* asset_paths[]) {
@@ -129,14 +126,12 @@ Cactus* create_cactus() {
     Cactus *cactus = malloc(sizeof(Cactus));
     int random = rand() % CACTUS_TYPES;
     Vector2 size = { textures[random].width, textures[random].height };
-    Rectangle cactus_source_rect = { 0, 0, textures[random].width, textures[random].height };
     Vector2 pos = { 1000.0f, INIT_GROUND_Y - textures[random].height + 20};
     Vector2 vel = { -5.0f, 0.0f };
     Rectangle cactus_pos_rect = { pos.x, pos.y, textures[random].width, textures[random].height };
 
     cactus->asset = textures[random];
     cactus->size = size;
-    cactus->cactus_source_rect = cactus_source_rect;
     cactus->pos = pos;
     cactus->vel = vel;
     cactus->cactus_pos_rect = cactus_pos_rect;
@@ -148,12 +143,10 @@ Ground* create_ground() {
     Ground *ground = malloc(sizeof(Ground));
     Vector2 vel = { GROUND_VEL_X, 0.0f };
     Vector2 pos = { 0.0f, INIT_GROUND_Y };
-    Rectangle ground_source_rect = {0, 0, textures[GROUND].width, textures[GROUND].height };
     
     ground->ground_frame = textures[GROUND];
     ground->vel = vel;
     ground->pos = pos;
-    ground->ground_source_rect = ground_source_rect;
 
     return ground;
 }
@@ -163,10 +156,8 @@ Dino* create_dino() {
     Vector2 size = { textures[DINO_RUN1].width, textures[DINO_RUN1].height };
     Vector2 vel = { 0.0f, 0.0f };
     Vector2 pos = { INIT_DINO_X, INIT_DINO_Y };
-    Rectangle dino_source_rect = {0, 0, textures[DINO_RUN1].width, textures[DINO_RUN1].height };
     Rectangle dino_pos_rect = { INIT_DINO_X, INIT_DINO_Y, textures[DINO_RUN1].width, textures[DINO_RUN1].height };
 
-    dino->dino_source_rect = dino_source_rect;
     dino->dino_pos_rect = dino_pos_rect;
     dino->dino_frame_loop[0] = textures[DINO_RUN1];
     dino->dino_frame_loop[1] = textures[DINO_RUN2];
@@ -266,14 +257,12 @@ int main() {
                 frames_counter = 0;
             }
 
-            dino->dino_source_rect.x = current_frame * dino->dino_frame_loop[current_frame].width;
-
-            DrawTextureRec(dino->dino_frame_loop[current_frame], dino->dino_source_rect, dino->pos, WHITE);
-            DrawTextureRec(ground->ground_frame, ground->ground_source_rect, ground->pos, WHITE);
-            DrawTextureRec(ground->ground_frame, ground->ground_source_rect, (Vector2){ground->pos.x + ground->ground_frame.width, ground->pos.y}, WHITE);
+            DrawTexture(dino->dino_frame_loop[current_frame], dino->pos.x, dino->pos.y, WHITE);
+            DrawTexture(ground->ground_frame, ground->pos.x, ground->pos.y, WHITE);
+            DrawTexture(ground->ground_frame, ground->pos.x + ground->ground_frame.width, ground->pos.y, WHITE);
 
             if (cactus != NULL) {
-                DrawTextureRec(cactus->asset, cactus->cactus_source_rect, cactus->pos, WHITE);
+                DrawTexture(cactus->asset, cactus->pos.x, cactus->pos.y, WHITE);
             }
 
             cactus_spawn_counter++;
@@ -287,7 +276,6 @@ int main() {
 
     unload_and_free(dino, cactus, retry, ground);
 
-    // TODO: Optimize game by loading assets only once
     // TODO: Handle retry button to start a new game (basically just switch game_over = false)
     // TODO: Optimize game by using just DrawTexture function as i have not a sprite but different png's
 
